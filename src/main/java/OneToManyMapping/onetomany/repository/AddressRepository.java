a0cc7de12831a62f25dd0b6f7bb9f32defa47d44
Muthu mari <muthumari8227@gmail.com>
2025-07-18 12:32:48 +0530

    Deleted specified files and folders

--- src/main/java/OneToManyMapping/onetomany/repository/AddressRepository.java
+++
@@ -1,8 +0,0 @@
-package OneToManyMapping.onetomany.repository;
-
-import OneToManyMapping.onetomany.entity.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
-
--- src/main/java/OneToManyMapping/onetomany/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package OneToManyMapping.onetomany.repository;
-
-import OneToManyMapping.onetomany.entity.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
